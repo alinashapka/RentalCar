c3b8e6a5675d0356b0eb94926074f6e1f061e3ca
alina <alinkash2811@gmail.com>
2025-09-10 11:40:25 +0200

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,23 @@
 import css from "./HomePage.module.css";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const goToCatalog = () => {
+    navigate("/catalog");
+  };
+
   return (
-    <>
-      <h2>I'm Home Page</h2>
-    </>
+    <div className={css.container}>
+      <h1 className={css.title}>Find your perfect rental car</h1>
+      <h2 className={css.text}>
+        Reliable and budget-friendly rentals for any journey
+      </h2>
+      <button className={css.button} onClick={goToCatalog}>
+        View Catalog
+      </button>
+    </div>
   );
 }
 
